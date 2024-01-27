c0710ffb0f97ada97b2612b1b0cafccfaeb22f3f
McWooden <garryf7887@gmail.com>
2024-01-27 19:10:24 +0700

    aplikasi readonly input dan daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     }
 
     return (
-        <div className="App">
+        <div className="App flex flex-col gap-2">
             <QrScanner
                 onScan={handleScan}
                 onError={handleError}
@@ -53,7 +53,17 @@ function App() {
                 }
                  */
             />
-            <p>{qrValue}</p>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">What is your name?</span>
+                <span className="label-text-alt">Top Right label</span>
+              </div>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" value={qrValue} readOnly/>
+              <div className="label">
+                <span className="label-text-alt">Bottom Left label</span>
+                <span className="label-text-alt">Bottom Right label</span>
+              </div>
+            </label>
         </div>
     );
 }
